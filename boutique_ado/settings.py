bbe4126038f0d0d6ba8d8b4993d30102865f8cc9
frank <masasimpafra@gmail.com>
2021-08-02 23:20:02 +0200

    add requirements

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['onlineboutique.herokuapp.com', '127.0.0.1', 'localhost']
 
 
@@ -205,8 +205,8 @@ DEFAULT_FILE_STORAGE ='custom_storages.MediaStorage'
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', 'whsec_OmpsJo8stQWA8c76UUUE4hxoWECnDfuH')
 
 if 'DEVELOPMENT' in os.environ:
